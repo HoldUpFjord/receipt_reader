d12d2035b2737869848551770ae6941cfd14797f
jackmonaco20 <jackmonaco20@yahoo.com>
2024-07-28 00:48:14 -0400

    added tailwind/shadcn/ got csv downloadable file

--- receipt_reader/src/app/page.tsx
+++ receipt_reader/src/app/page.tsx
@@ -1,12 +1,13 @@
 "use client"
 import Image from "next/image";
-
+import { Button } from "@/components/ui/button";
 interface Receipt {
   m:string
 }
 interface MyButtonComponentProps {
   onClick: () => void;
 }
+type ContentType = 'text/plain' | 'text/html' | 'text/csv' | 'application/json' | 'application/pdf' | string;
 
 let receipt = async ():Promise<string> => {
   const { createWorker } = require('tesseract.js');
@@ -55,12 +56,27 @@ const handleProcess = async () => {
   }
 };
 
+function downloadFile(content:string, filename:string, contentType:ContentType) {
+  const blob = new Blob([content], { type: contentType });
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement('a');
+  link.href = url;
+  link.download = filename;
+  link.click();
+  URL.revokeObjectURL(url);
+}
+
+// Usage
 export default function Home() {
-  
+  const handleDownload = () => {
+    downloadFile('Hello, World!', 'hello.csv', 'text/csv');
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <MyButtonComponent onClick={handleProcess} />
       {/* <MyButtonComponent onClick={openai} /> */}
+      <Button onClick={handleDownload}>Download</Button>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
